e93764705b5264a930e5b5a4dced3a1ee0de8bb6
piyushhpandit <piyushhpandit@users.noreply.github.com>
2026-02-06 04:46:40 +0000

    deploy: 6abfd66e6d8ee558101122b001517f650f58857e

---
+++ _next/static/0qSSZIWP92GX5QZeM9954/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/newfriendssaloon//_next/:path+",
+        "destination": "/newfriendssaloon/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
